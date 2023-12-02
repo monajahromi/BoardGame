7dd4be1972e2548c66423f49a3bdbad8d5ad4d62
Mona Jahromi <62639083+monajahromi@users.noreply.github.com>
2023-12-03 01:05:20 +0330

    config added

---
+++ src/main/java/com/game/mancala/utils/MancalaConfig.java
@@ -0,0 +1,15 @@
+package com.game.mancala.utils;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties("game.mancala")
+@Data
+public class MancalaConfig {
+
+  private int default_player_pits_count;
+  private int default_stones_per_pit;
+
+}
