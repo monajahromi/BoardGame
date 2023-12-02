8eec4f7b8f8e06cdefbac3ccd16dab133e3e4896
Mona Jahromi <62639083+monajahromi@users.noreply.github.com>
2023-12-03 00:37:40 +0330

    game controller added

---
+++ src/main/java/com/game/mancala/controller/MancalaController.java
@@ -0,0 +1,32 @@
+package com.game.mancala.controller;
+
+import com.game.mancala.model.entity.GameEntity;
+import com.game.mancala.service.GameService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/mancala")
+@Validated
+public class MancalaController {
+    private final GameService gameService ;
+
+    public MancalaController(GameService gameService) {
+        this.gameService = gameService;
+    }
+
+
+    @PostMapping("start")
+    public ResponseEntity<GameEntity> create(@RequestBody GameEntity game) {
+        return new ResponseEntity<>(gameService.create(game), HttpStatus.CREATED);
+    }
+
+    @PutMapping("play")
+    public ResponseEntity<GameEntity> play(@RequestBody GameEntity game)  {
+        return new ResponseEntity<>(gameService.play(game), HttpStatus.CREATED);
+    }
+
+}
