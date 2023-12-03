cab87559bf9475a3a40e20d0757e29bed9bf9323
Mona Jahromi <62639083+monajahromi@users.noreply.github.com>
2023-12-04 03:24:33 +0330

    Add getGameMatrix method to GameRule
    obtaining the game matrix from a GameEntity instance.

--- src/main/java/com/game/mancala/rule/GameRule.java
+++ src/main/java/com/game/mancala/rule/GameRule.java
@@ -5,6 +5,8 @@ import com.game.mancala.model.entity.GameEntity;
 import com.game.mancala.model.entity.Player;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
+
 @Service
 public interface GameRule {
 
@@ -22,6 +24,19 @@ public interface GameRule {
 
         int distanceToBigPit = game.getPitsPerPlayer() - selectedPit;
         return stoneCounts % game.getTotalPits() != distanceToBigPit;
+    }
+
+    default int[][] getGameMatrix(GameEntity game) {
+        return game.getPlayers().stream()
+                .map(element -> {
+                    // playerRow is consist of player pits and its bigPit
+                    int[] playerRowIntGameMatrix = Arrays.copyOf(element.getPits(),
+                            game.getPitsPerPlayer() + 1);
+                    //last item of player row is its big pit
+                    playerRowIntGameMatrix[game.getPitsPerPlayer()] = element.getBigPit();
+                    return playerRowIntGameMatrix;
+
+                }).toArray(int[][]::new);
     }
 
 }
