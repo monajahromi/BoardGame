9e9e12530956c70ec2c44e15d22c444503d358ae
Mona Jahromi <62639083+monajahromi@users.noreply.github.com>
2023-12-04 02:58:02 +0330

    IndexGenerator class

---
+++ src/main/java/com/game/mancala/rule/IndexGenerator.java
@@ -0,0 +1,64 @@
+package com.game.mancala.rule;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class IndexGenerator {
+
+
+    // Generates indexes based on the provided parameters.
+    public static List<int[]> generateIndexes(int rowNum, int colNum,
+                                              int selectedRow, int selectedCol,
+                                              int stoneCount) {
+        List<int[]> indexes = new ArrayList<>();
+
+        // generates indexes for first row
+        indexes.addAll(IntStream.range(selectedCol + 1, colNum)
+                .mapToObj(i -> new int[]{selectedRow, i})
+                .collect(Collectors.toList()));
+
+        // generate index for remaining stones,
+        int currentRow = (selectedRow + 1) % rowNum;
+        indexes.addAll(IntStream.iterate(currentRow, i -> (i + 1) % rowNum)
+                .mapToObj(element -> (element == selectedRow) ?
+                        generateRowIndexes(element, colNum) :
+                        generateReverseRowIndexes(element, colNum))
+                .flatMap(List::stream)
+                .limit(stoneCount)
+                .collect(Collectors.toList()));
+
+        return indexes;
+    }
+
+    // generates index for specific row and its columns
+    private static List<int[]> generateRowIndexes(int row, int colNum) {
+        return IntStream.range(0, colNum)
+                .mapToObj(j -> new int[]{row, j})
+                .collect(Collectors.toList());
+    }
+
+    //generate index for specific row in reverse order, including last column
+    private static List<int[]> generateReverseRowIndexes(int row, int colNum) {
+        List<int[]> a = IntStream.rangeClosed(0, colNum - 2)
+                .mapToObj(j -> new int[]{row, colNum - 2 - j})
+                .collect(Collectors.toList());
+        a.add(new int[]{row, colNum - 1});
+        return a;
+    }
+
+
+    public static void main(String[] args) {
+        int rowNum = 2;
+        int colNum = 7;
+        int selectedRow = 1;
+        int selectedCol = 4;
+        int stoneCount = 15;
+
+        List<int[]> result = generateIndexes(rowNum, colNum, selectedRow, selectedCol, stoneCount);
+        result.forEach(item -> System.out.println(Arrays.toString(item)));
+    }
+
+}
