6941cf520a7556cf9491c8ed7838bd79d29c40ca
Mona Jahromi <62639083+monajahromi@users.noreply.github.com>
2023-12-08 18:58:02 +0330

    Refactor application and project names

--- src/main/java/com/game/BoardGameApplication.java
+++ src/main/java/com/game/BoardGameApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MancalaApplication {
+public class BoardGameApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MancalaApplication.class, args);
+        SpringApplication.run(BoardGameApplication.class, args);
     }
 
 }
